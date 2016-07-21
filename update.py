d1336554927d249338c989b7f83d2340d2ef499f
Tankobot <tankobot@users.noreply.github.com>
2016-07-20 17:30:20 -0700

    Add thinking wheel to notify user of connecting state.

--- update.py
+++ update.py
@@ -1,4 +1,6 @@
 import json
+import threading
+from time import sleep
 from tarfile import open as tar
 from tempfile import TemporaryFile
 from binascii import hexlify
@@ -56,6 +58,7 @@ def calculate_hash(force=False):
     for path in sorted(py_files):
         with py_files[path].open('rb') as file:
             print('Hashing %s' % path)
+            sleep(wait)
             for line in file:
                 sha.update(line)
     h = hexlify(sha.digest()).decode('utf-8')
@@ -71,7 +74,57 @@ def grab_version(latest=False):
         url = 'https://tankobot.github.io' \
               '/p2pg/version.json'
     print('Getting version information...')
-    return get(url).json()
+    wheel = Wheel()
+    wheel.start()
+    result = get(url).json()
+    wheel.stop()
+    return result
+
+
+class Wheel:
+    def __init__(self, speed=4, clockwise=True):
+        self.speed = speed
+        self.clockwise = clockwise
+        self._state_lock = threading.Lock()
+        self._state = False
+        self._thread = None
+
+    @property
+    def state(self):
+        with self._state_lock:
+            return self._state
+
+    @state.setter
+    def state(self, value):
+        with self._state_lock:
+            self._state = bool(value)
+
+    def loop(self):
+        place = 0
+        frames = ('-', '\\', '|', '/')
+        direction = 1 if self.clockwise else -1
+        while self.state:
+            print(' ', frames[place], end='\r')
+            place += direction
+            place %= len(frames)
+            sleep(1 / self.speed)
+        print('   ', end='\r')
+
+    def start(self):
+        self.state = True
+        if self._thread:
+            raise Error('wheel already started')
+        else:
+            self._thread = threading.Thread(target=self.loop)
+            self._thread.start()
+
+    def stop(self):
+        self.state = False
+        self._thread.join(1)
+        if self._thread.is_alive():
+            raise Error('wheel did not terminate')
+        else:
+            self._thread = None
 
 
 def download_version(version: str = None, v_info=None, chunk_size=128):
@@ -87,10 +140,16 @@ def download_version(version: str = None, v_info=None, chunk_size=128):
         url = v_info[version][1]
     if v['current'] != n_sig:
         print('Downloading version (%s)' % version)
+        wheel = Wheel()
+        wheel.start()
         new_version = get(url, stream=True)
         file = TemporaryFile(buffering=0)
-        total = int(new_version.headers['content-length']) or 1
+        try:
+            total = int(new_version.headers['content-length'])
+        except KeyError:
+            total = 1
         place = 0
+        wheel.stop()
         for chunk in new_version.iter_content(chunk_size):
             file.write(chunk)
             place += len(chunk)
@@ -107,6 +166,7 @@ def remove_folder(tar_file):
         tar_info.name = tar_info.name.partition('/')[2]
         if tar_info.name:
             print('Extracting %s' % tar_info.name)
+            sleep(wait)
             yield tar_info
         tar_info = tar_file.next()
 
@@ -121,16 +181,29 @@ def install(version: str, v_info=None, chunk_size=None):
         archive.close()
 
 
+wait = 0
+
+
 def main():
     from argparse import ArgumentParser
     parser = ArgumentParser(description='Update p2pg.')
-    parser.add_argument('-c', '--chunk-size', dest='chunk_size',
+    parser.add_argument('-j', '--just-hash', dest='just_hash',
+                        action='store_true', help='only display/write hash')
+    parser.add_argument('-c', '--chunk', dest='chunk_size',
+                        metavar='INT',
                         type=int, help='chunk size')
+    parser.add_argument('-w', '--wait', dest='wait',
+                        default=0, type=float,
+                        help='seconds to wait between actions')
     args = parser.parse_args()
+    global wait
+    wait = args.wait
 
     print('Calculating hash...')
     _sha = calculate_hash(True)
     print('   sha: %s' % _sha)
+    if args.just_hash:
+        return
     if input('Update y/[n]: ') == 'y':
         v_default = v['get-version']
         wanted = input('Version [%s]: ' % v_default).strip() or v_default
